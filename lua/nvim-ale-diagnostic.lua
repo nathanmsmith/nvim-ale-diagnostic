5e0c0361d4d15218c12c03c55e89744b5ec633dc
Esteban Beltran <sergies@gmail.com>
2021-10-07 12:00:38 +0200

    Update to support api_level 8 in neovim 0.6

--- lua/nvim-ale-diagnostic.lua
+++ lua/nvim-ale-diagnostic.lua
@@ -12,23 +12,54 @@ vim.lsp.diagnostic.clear = function(bufnr, client_id, diagnostic_ns, sign_ns)
   vim.api.nvim_call_function('ale#other_source#ShowResults', {bufnr, "nvim-lsp", {}})
 end
 
-vim.lsp.diagnostic.set_signs = function(diagnostics, bufnr, _, _, _)
-  if not diagnostics then
-    return
+if vim.version().api_level == 8 then
+  local function set_signs(bufnr)
+      -- Get all diagnostics from the current buffer
+    local diagnostics = vim.diagnostic.get(bufnr)
+    local items = {}
+
+    for _, item in ipairs(diagnostics) do
+      local nr = ''
+      if item.user_data and item.user_data.lsp and item.user_data.lsp.code then
+        nr = item.user_data.lsp.code
+      end
+      table.insert(items, {
+        nr = nr,
+        text = item.message,
+        lnum = item.lnum+1,
+        end_lnum = item.end_lnum,
+        col = item.col,
+        end_col = item.end_col,
+        type = ale_diagnostic_severity_map[item.severity]
+      })
+    end
+
+    vim.api.nvim_call_function('ale#other_source#ShowResults', {bufnr, "nvim-lsp", items})
   end
 
-  local items = {}
-  for _, item in ipairs(diagnostics) do
-    table.insert(items, {
-      nr = item.code,
-      text = item.message,
-      lnum = item.range.start.line+1,
-      end_lnum = item.range['end'].line,
-      col = item.range.start.character+1,
-      end_col = item.range['end'].character,
-      type = ale_diagnostic_severity_map[item.severity]
-    })
+  function vim.diagnostic.show(namespace, bufnr, ...)
+    set_signs(bufnr)
   end
+else
+  vim.lsp.diagnostic.set_signs = function(diagnostics, bufnr, _, _, _)
+    if not diagnostics then
+      return
+    end
 
-  vim.api.nvim_call_function('ale#other_source#ShowResults', {bufnr, "nvim-lsp", items})
+    local items = {}
+    for _, item in ipairs(diagnostics) do
+      table.insert(items, {
+        nr = item.code,
+        text = item.message,
+        lnum = item.range.start.line+1,
+        end_lnum = item.range['end'].line,
+        col = item.range.start.character+1,
+        end_col = item.range['end'].character,
+        type = ale_diagnostic_severity_map[item.severity]
+      })
+    end
+
+    vim.api.nvim_call_function('ale#other_source#ShowResults', {bufnr, "nvim-lsp", items})
+  end
 end
+
